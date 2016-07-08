2e0ee34634a62de8d01c1102aac66c74071938c6
Teddy Fontaine <teddy.fontaine@epitech.eu>
2016-07-08 13:26:29 +0200

    Init pin before test

--- src/Plugin/Motor/MotorController.java
+++ src/Plugin/Motor/MotorController.java
@@ -7,7 +7,6 @@ import Core.Http.Oauth2;
 import Core.Methode;
 import Core.Route;
 import Plugin.Motor.Model.MotorModel;
-import com.pi4j.wiringpi.SoftPwm;
 import org.json.JSONObject;
 
 /**
@@ -18,7 +17,6 @@ public class MotorController {
     @Methode("GET")
     @Route("/motor/set/{pin}/{speed}")
     public MotorModel setMotor(String socket, Oauth2 oauth2, Header header, JSONObject jsonObject, Map args) {
-        SoftPwm.softPwmCreate(args.getInt("pin"), 0, 100);
         return new MotorModel().setSpeed(socket, args.getInt("pin"), args.getInt("speed"));
     }
 }

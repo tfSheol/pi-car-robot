c125030f5e7bd90cc7a5848c5f7e1d4dded18059
Teddy Fontaine <teddy.fontaine@epitech.eu>
2016-07-08 23:57:43 +0200

    Update system fix

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,8 +3,10 @@ sleep 5
 sudo pkill -f 'java -jar'
 sleep 5
 cd ../
+sudo rm -rf ./target
 sudo -u pi git pull
-sudo mvn clean compile package
+sleep 10
+sudo -u pi mvn clean compile package
 cd ./target
 sudo chmod 744 ./launcher.sh
 sleep 5

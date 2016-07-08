76791f20e31ac962473eb9151214cadb93299287
Teddy Fontaine <teddy.fontaine@epitech.eu>
2016-07-08 23:43:27 +0200

    Fix

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+sleep 5
 sudo pkill -f 'java -jar'
+sleep 5
 cd ../
 sudo -u pi git pull
 sudo mvn clean compile package
 cd ./target
 sudo chmod 744 ./launcher.sh
+sleep 5
 sudo ./launcher.sh

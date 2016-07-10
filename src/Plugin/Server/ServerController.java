100cc22913bd9d98c66a776019f04262f842107c
Teddy Fontaine <teddy.fontaine@epitech.eu>
2016-07-10 23:00:23 +0200

    jeej

--- src/Plugin/Server/ServerController.java
+++ src/Plugin/Server/ServerController.java
@@ -40,6 +40,7 @@ public class ServerController {
             SoftPwm.softPwmWrite(2, 100);
             Runtime runtime = Runtime.getRuntime();
             runtime.exec("sudo -u pi git pull");
+            runtime.exec("cd ../");
             Process p = runtime.exec("sudo mvn clean compile package");
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(p.getInputStream()));
